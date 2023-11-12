e27abbcf4e770f5009e43a6ce5ade0815b15fe49
Zequi009 <ezeceruti@gmail.com>
2023-11-12 20:48:59 -0300

    12/11/2023 POST de datos a la Api

--- Css-Js/NuevaCarpeta.js
+++ Css-Js/NuevaCarpeta.js
@@ -3,28 +3,33 @@ document.getElementById('formulario').addEventListener('submit', function (event
 
     const descripcion = document.getElementById('descripcion').value;
     const descripcionPlan = document.getElementById('descripcionPlan').value;
-    const planInicio = document.getElementById('plan_start_date').value;
-    const planFin = document.getElementById('plan_end_date').value;
+    const plan_start_date = document.getElementById('plan_start_date').value;
+    const plan_end_date = document.getElementById('plan_end_date').value;
 
-    // Formatear las fechas a yyyy-mm-dd
-    const inicioFormateado = new Date(planInicio.split("/").reverse().join("-")).toISOString().split("T")[0];
-    const finFormateado = new Date(planFin.split("/").reverse().join("-")).toISOString().split("T")[0];
-
-    console.log('Datos a enviar a la Api:', {
-        descripcion,
-        descripcionPlan,
-        planInicio: inicioFormateado,
-        planFin: finFormateado,
-    });
 
     // Crear un objeto formData con los datos a enviar
     const formData = {
         DESCRIPCION: descripcion,
         PLAN_CARRERA: descripcionPlan,
-        PLAN_START_DATE: inicioFormateado,
-        PLAN_END_DATE: finFormateado,
     };
 
+    //Si los campos de Fechas con datos los conviertea al formato de la base de datos
+    if(plan_start_date){
+        const inicioFormateado = new Date(plan_start_date.split("/").reverse().join("-")).toISOString().split("T")[0];
+        formData.PLAN_START_DATE = inicioFormateado;
+    }
+    if(plan_end_date){
+        const finFormateado = new Date(plan_end_date.split("/").reverse().join("-")).toISOString().split("T")[0];
+        formData.PLAN_END_DATE=finFormateado;
+    }
+
+    console.log('Datos a enviar a la Api:', {
+        descripcion,
+        descripcionPlan,
+        plan_start_date: formData.PLAN_START_DATE,
+        plan_end_date: formData.PLAN_END_DATE,
+    });
+    
     fetch('http://localhost:8000/API/CARRERAS/add', {
         method: 'POST',
         headers: {
@@ -37,15 +42,23 @@ document.getElementById('formulario').addEventListener('submit', function (event
             throw new Error('Error al enviar el formulario a la Api');
         }
         return response.json();
+        //Mostrar Toas cuando los datos se guardan
+
     })
     .then(data => {
         console.log('Respuesta de la Api:', data);
-
-        
+        //Mostar Toast de exito
+        const toastSuccess = document.getElementById('toastSuccess');
+        const toastSS = new bootstrap.Toast(toastSuccess);
+        toastSS.show();
     })
     .catch(error => {
         console.error('Error:', error.message);
         console.error('Detalle del Error:', error.response); // Muestra los detalles del Error
 
+        //Mostrar toast de Error
+        const toastError = document.getElementById('toastError');;
+        const terror = new bootstrap.Toast(toastError);
+        terror.show();
     });
 });
